80f079d518567a29209d9b2ede857103f07afea4
jose <morelosjosealfredo@gmail.com>
2021-12-02 18:17:43 -0500

    make it better

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    server: 'https://creatibook-datos.herokuapp.com/',
-    client: 'https://creatibook-front.herokuapp.com/',
+    server: 'https://creatibook-datos.herokuapp.com',
+    client: 'https://creatibook-front.herokuapp.com',
     URI: 'mongodb+srv://Test-admin:test-admin@clustertest.ezgdr.mongodb.net/creatibook?retryWrites=true&w=majority',
     key: 'clavesecreta2021'
 }
